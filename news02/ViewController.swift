f062e2498dc7b6e68f6cd45ac152eeb73fc79a10
webdmitriev <webdmitriev@gmail.com>
2024-12-02 20:49:36 +0300

    Initial Commit

---
+++ news02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news02
+//
+//  Created by Олег Дмитриев on 02.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
